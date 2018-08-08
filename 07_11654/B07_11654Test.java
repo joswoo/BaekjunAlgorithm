d9b9831b7f6378740bd762916e732a627f0b3dd9
joswoo <josw1217@naver.com>
2018-08-09 00:13:21 +0900

    update 07_11654

--- 07_11654/B07_11654Test.java
+++ 07_11654/B07_11654Test.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -8,18 +9,13 @@ class B07_11654Test {
 	@Test
 	void test() {
 		B07_11654 B07_11654Test = new B07_11654();
-		String A = "A";
-		String C = "C";
-		String zero = "0";
-		String nine = "9";
-		String a = "a";
-		String z = "z";
-		assertSame(65, B07_11654Test.ascii(A));
-		assertSame(67, B07_11654Test.ascii(C));
-		assertSame(48, B07_11654Test.ascii(zero));
-		assertSame(57, B07_11654Test.ascii(nine));
-		assertSame(97, B07_11654Test.ascii(a));
-		assertSame(122, B07_11654Test.ascii(z));
+		assertEquals(65,B07_11654Test.ascii("A"));
+		assertEquals(67,B07_11654Test.ascii("C"));
+		assertEquals(48,B07_11654Test.ascii("0"));
+		assertEquals(57,B07_11654Test.ascii("9"));
+		assertEquals(97,B07_11654Test.ascii("a"));
+		assertEquals(122,B07_11654Test.ascii("z"));
+		
 		
 	}
 
